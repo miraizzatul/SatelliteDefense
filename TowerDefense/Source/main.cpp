76e73f670dc834b3fafcd5ea6310e80096115ac6
Mira <39260938+miramuzafar@users.noreply.github.com>
2025-07-05 22:10:42 +0800

    Tower Defense gameplay update
    - Fixed keyboard movement wasd not working
    - Added smooth movement to the green tower
    - Added simple grid tiles

--- TowerDefense/Source/main.cpp
+++ TowerDefense/Source/main.cpp
@@ -4,6 +4,7 @@ int main(int argc, char* argv[])
 {
 	SDL_Init(SDL_INIT_VIDEO); //initialize sdl3
 
+	//---------------------window setup----------------------//
 	//Create sdl3 window (80*600)
 	SDL_Window* Window = SDL_CreateWindow("Tower Defense",800, 600, SDL_EVENT_WINDOW_SHOWN);
 
@@ -35,11 +36,35 @@ int main(int argc, char* argv[])
 	bool running = true;
 	SDL_Event event;
 
+	//------------------end window setup--------------------------//
+
+	//----------------------------------------------input movements
+
+	bool keys[SDL_SCANCODE_COUNT] = { false };//track held keys
+	Uint64 now = SDL_GetPerformanceCounter();// get timestamp
+	Uint64 last = 0;
+	float deltaTime = 0.f;
+	float speed = 200.f; // pixels per second
+
+	//-------------------------------------------end input movements
+
+	//Define grid settings
+	const int tileCols = 10;
+	const int tileRows = 8;
+	const float tileWidth = 800.f / tileCols;
+	const float tileHeight = 600.f / tileRows;
+
 	//set rectangle location and size (x,y,w,h)
 	SDL_FRect Tower = { 100.f, 250.f, 50.f, 100.f };
 
 	while (running) //keeps the window running
 	{
+		SDL_Delay(1);//prevent the loop from hogging the cpu since sometimes cpu might run too fast and render frames too quicky
+
+		last = now;
+		now = SDL_GetPerformanceCounter();//current timestamp
+		deltaTime = (now - last) / (float)SDL_GetPerformanceFrequency(); //SDL_GetPerformanceFrequency gives us number of ticks per second
+
 		while (SDL_PollEvent(&event))
 		{
 			if (event.type == SDL_EVENT_QUIT)//checks for quit events (like clicking the X button)
@@ -48,43 +73,59 @@ int main(int argc, char* argv[])
 			}
 			if (event.type == SDL_EVENT_KEY_DOWN)
 			{
+				keys[event.key.scancode] = true;
 				if (event.key.scancode == SDL_SCANCODE_ESCAPE) //check for keyboard input if escape key is pressed
 				{
 					SDL_Log("Escape key pressed — exiting...");
 					running = false;
 				}
-				switch (event.key.scancode) //mnoving the tower using wasd key
-				{
-					case SDL_SCANCODE_UP:
-						Tower.y -= 5;//move up
-						break;
-					case SDL_SCANCODE_DOWN:
-						Tower.y += 5;//move down
-						break;
-					case SDL_SCANCODE_LEFT:
-						Tower.x -= 5;//move left
-						break;
-					case SDL_SCANCODE_RIGHT:
-						Tower.x += 5;//move right
-						break;
-				}
-
-				//Clamp the tower from going offscreen
-				if (Tower.x < 0) Tower.x = 0;//left edge
-				if (Tower.y < 0) Tower.y = 0;//top edge
-				if (Tower.x + Tower.w > 800)Tower.x = 800 - Tower.w;//(Tower.x+Tower.w) right edge of rectangle
-				if (Tower.y + Tower.h > 600)Tower.y = 600 - Tower.h;//(Tower.y+Tower.h) bottom edge of rectangle
+			}
+			else if (event.type == SDL_EVENT_KEY_UP)
+			{
+				keys[event.key.scancode] = false;
 			}
 		}
+		//movement logic
+		if (keys[SDL_SCANCODE_W] || keys[SDL_SCANCODE_UP]) Tower.y -= speed * deltaTime;
+		if (keys[SDL_SCANCODE_S] || keys[SDL_SCANCODE_DOWN]) Tower.y += speed * deltaTime;
+		if (keys[SDL_SCANCODE_A] || keys[SDL_SCANCODE_LEFT]) Tower.x -= speed * deltaTime;
+		if (keys[SDL_SCANCODE_D] || keys[SDL_SCANCODE_RIGHT]) Tower.x += speed * deltaTime;
+
+		//Clamp the tower from going offscreen
+		if (Tower.x < 0) Tower.x = 0;//left edge
+		if (Tower.y < 0) Tower.y = 0;//top edge
+		if (Tower.x + Tower.w > 800)Tower.x = 800 - Tower.w;//(Tower.x+Tower.w) right edge of rectangle
+		if (Tower.y + Tower.h > 600)Tower.y = 600 - Tower.h;//(Tower.y+Tower.h) bottom edge of rectangle
+
 		SDL_SetRenderDrawColor(Renderer, 30, 30, 30, 255);//set draw colour dark grey
 		SDL_RenderClear(Renderer);//clear the screen with grey colour
 
+		//draw simple grid tiles
+		for (int row = 0; row < tileRows; ++row)
+		{
+			for (int col = 0; col < tileCols; ++col)
+			{
+				SDL_FRect tileRect = {
+					col * tileWidth,
+					row * tileHeight,
+					tileWidth,
+					tileHeight
+				};
+
+				//Alternate light and dark tiles
+				if((row+col) %2 ==0)
+					SDL_SetRenderDrawColor(Renderer, 220, 220, 220, 255); // light gray
+				else
+					SDL_SetRenderDrawColor(Renderer, 180, 180, 180, 255); // dark gray
+
+				SDL_RenderFillRect(Renderer, &tileRect);
+			}
+		}
 		//create green tower colour
 		SDL_SetRenderDrawColor(Renderer, 0, 200, 0, 255);
 
-		//create rectangle
+		//render the tower on screen
 		SDL_RenderFillRect(Renderer, &Tower);
-
 
 		SDL_RenderPresent(Renderer);//present the result to the window
 	}
